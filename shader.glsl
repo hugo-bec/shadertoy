30e3cb9dbdef8e83ae155ed5c9ca04fe0c67c006
hugo-bec <bechugo1@outlook.fr>
2024-11-17 21:48:28 +0100

    add random point on sphere

--- shader.glsl
+++ shader.glsl
@@ -11,10 +11,33 @@ const vec3 BACK_COLOR = vec3(0.8);
 const vec3 LIGHT_POS = vec3(2., 2., 4.);
 const float GI_SIM = 0.4; //global illumination simulation (between 0. and 1.)
 
+const float M_PI = 3.14159265358979323846;
+const float FLT_MAX = 3.402823466e+38;
+
 vec3 A = vec3(-1,-1,0);
 vec3 B = vec3(1,-1,0);
 vec3 C = vec3(0,1,0);
 vec3 D = vec3(0,0,-1);
+
+uint uhash( uint x ) {
+    x += ( x << 10u );
+    x ^= ( x >>  6u );
+    x += ( x <<  3u );
+    x ^= ( x >> 11u );
+    x += ( x << 15u );
+    return x;
+}
+
+float rand( uint x ) {
+    return float(uhash(x)) / float(0xffffffff);
+}
+
+vec3 spherical2cartesian(float theta, float phi, float r){
+    return vec3(r * sin(theta) * cos(phi),
+                r * sin(theta) * sin(phi),
+                r * cos(theta));
+}
+
 
 vec4 dist_sphere(vec3 p, vec3 center, float r, vec3 color){
     return vec4(length(p - center) - r, color);
@@ -35,22 +58,44 @@ vec4 guingangou(vec3 p, vec3 color)
     float dBD = abs(dB - dD);
     float dCD = abs(dC - dD);
 
-    float d = (dAB + dAC + dAD + dBC + dBD + dCD) - (sin(iTime) + 1.);
+    float d = (dAB + dAC + dAD + dBC + dBD + dCD) - (sin(1.) + 1.) * 0.1;
 
     return vec4(d, color);
 }
 
+vec4 guingangougan(vec3 p, vec3 color)
+{
+    float dmin = FLT_MAX;
+    uint rnd = 3u;
+    for(int i=0; i<10; i++){
+        rnd = uhash(rnd);
+        float theta = (float(rnd) / float(0xffffffff)) * (M_PI / 2.);
+        rnd = uhash(rnd);
+        float phi = (float(rnd) / float(0xffffffff)) * (M_PI * 2.);
+
+        vec3 pi = spherical2cartesian(theta, phi, 1.);
+        float di = distance(p, pi) - 0.05;
+        dmin = di < dmin ? di : dmin;
+    }
+    return vec4(dmin, color);
+}
+
 vec4 sdScene(vec3 p){
-    vec4 dmin   = dist_sphere(p, A, 0.1, vec3(1., 1., 0.));
-    vec4 d1     = dist_sphere(p, B, 0.1, vec3(1., 1., 0.));
-    dmin = d1.x < dmin.x ? d1 : dmin;
-    vec4 d2     = dist_sphere(p, C, 0.1, vec3(1., 1., 0.));
-    dmin = d2.x < dmin.x ? d2 : dmin;
-    vec4 d3     = dist_sphere(p, D, 0.1, vec3(1., 1., 0.));
-    dmin = d3.x < dmin.x ? d3 : dmin;
+    vec4 dmin = vec4(FLT_MAX, vec3(0));
+    //vec4 d0   = dist_sphere(p, A, 0.1, vec3(1., 1., 0.));
+    //dmin = d0.x < dmin.x ? d0 : dmin;
+    //vec4 d1     = dist_sphere(p, B, 0.1, vec3(1., 1., 0.));
+    //dmin = d1.x < dmin.x ? d1 : dmin;
+    //vec4 d2     = dist_sphere(p, C, 0.1, vec3(1., 1., 0.));
+    //dmin = d2.x < dmin.x ? d2 : dmin;
+    //vec4 d3     = dist_sphere(p, D, 0.1, vec3(1., 1., 0.));
+    //dmin = d3.x < dmin.x ? d3 : dmin;
     
     vec4 dhex = guingangou(p, vec3(1., 0., 0.));
     dmin = dhex.x < dmin.x ? dhex : dmin;
+
+    vec4 drnd = guingangougan(p, vec3(0., 0., 1.));
+    dmin = drnd.x < dmin.x ? drnd : dmin;
 
     return dmin;
 }
